46c1fcb556068fcf1dfd84622b532b173a8eaf7a
paulst30 <paul.stricker@rub.de>
2023-06-02 10:02:14 +0200

    many changes to setup and estimatation

--- Setup.R
+++ Setup.R
@@ -16,6 +16,13 @@
 #install.packages("xgboost")
 #install.packages("Matrix")
 #install.packages("cowplot")
+#install.packages("FactoMineR")
+#install.packages("car")
+#install.packages("glmnet")
+#install.packages("RANN")
+#install.packages("diversityForest")
+#install.packages("stringr")
+
 
 library(tidyverse)
 library(ggplot2)
@@ -36,13 +43,63 @@ library(xtable)
 library(xgboost)
 library(Matrix)
 library(cowplot)
+library(corrplot)
+library(FactoMineR)
+library(car)
+library(glmnet)
+library(RANN)
+library(diversityForest)
+library(stringr)
 
 #read in Stata data
 data <- read_dta('quality_analysis_ml_data.dta')
 
+#impute
 
-#prediction data
-p_data <- data %>% filter(!is.na(OUT_BMD4) & is.na(IN_BMD4)) %>%
+
+#build additional features 
+
+#calculate weights of observations
+data <- data %>% group_by(des_pair) %>% mutate(max_OUT=max(OUT_BMD4,na.rm=T)+1,
+                                               OUT_BMD4=OUT_BMD4,
+                                               re_IN_BMD4=re_IN_BMD4,
+                                               new_diff=diff_inBMD4_outBMD4) %>% ungroup()
+
+
+
+data <- data %>% mutate( s_out_fellow = case_when(!is.na(OUT_N_fellow/OUT_BMD4) ~ OUT_N_fellow/OUT_BMD4 ,OUT_N_fellow==0 & OUT_BMD4==0 ~ 0),
+                         s_out_fellow2 = IMF_OUT_net_fellow/OUT_BMD4,
+                         s_re_in_fellow = re_IMF_IN_net_fellow/re_IN_BMD4,
+                         s_out_spe = OECD_OUT_SPE/OUT_BMD4,
+                         s_re_in_spe = re_OECD_IN_SPE/re_IN_BMD4,
+                         s_in_spe_IIP = SPE_IIP_inward/IIP_inward,
+                         s_out_spe_IIP =SPE_IIP_outward/IIP_outward) %>%
+                         group_by(des_pair) %>%
+                         mutate(lag1 = lag(new_diff, n=1L),
+                         lag2 = lag(new_diff, n=2L),
+                         lead1 = lead(new_diff, n=1L),
+                         lead2 = lead(new_diff, n=2L),
+                         n_group = sum(!is.na(OUT_BMD4)),
+                         n_zero = sum(OUT_BMD4==0, na.rm = T),
+                         mean_group = mean(OUT_BMD4, na.rm = T),
+                         sd_group = sd(OUT_BMD4, na.rm = T)) %>%
+                         ungroup()
+
+#additional numeric features
+
+
+#####################select variables to work with##############################
+set.seed(100)
+working_data_wm <- data %>% select(-Type, -re_IMF_IN, -re_OECD_IN_BMD4) %>%
+  mutate_if(is.character, as.factor) %>%
+  filter(!is.na(OUT_BMD4))   # Outward stocks data needs to be non-missing
+
+modelling_data <- working_data_wm %>% filter(!is.na(OUT_BMD4) & !is.na(IN_BMD4))
+prediction_data <- working_data_wm %>% filter(!is.na(OUT_BMD4) & is.na(IN_BMD4))
+
+
+# check the missingness of data
+p_data <- prediction_data %>%
   select( -starts_with("IMF"), -IIA, -PTA, -DTT, -BIT, -exchange_rate, -ind_exchange_rate,
           -fin_center, -s_id, -r_id, -group_id, -s_answers, -r_answers, -s_iso3c, -r_iso3c, -des_pair, -re_des_pair,
           -ends_with("14"), -ends_with("17"), -ends_with("18"), -ends_with("19"), -ends_with("20"),
@@ -50,8 +107,9 @@ p_data <- data %>% filter(!is.na(OUT_BMD4) & is.na(IN_BMD4)) %>%
   summarise(across(everything(),~ mean(!is.na(.x))))
 rownames(p_data) <- c("Prediction")
 p_data <- t(p_data)
+
 #training data
-t_data <- data %>% filter(!is.na(OUT_BMD4) & !is.na(IN_BMD4)) %>%
+t_data <- modelling_data %>%
   select( -starts_with("IMF"), -IIA, -PTA, -DTT, -BIT, -exchange_rate, -ind_exchange_rate,
           -fin_center, -s_id, -r_id, -group_id, -s_answers, -r_answers, -s_iso3c, -r_iso3c, -des_pair,-re_des_pair,
           -ends_with("14"), -ends_with("17"), -ends_with("18"), -ends_with("19"), -ends_with("20"),
@@ -62,72 +120,169 @@ t_data <- t(t_data)
 
 #combining both
 coverage <- cbind(t_data,p_data)
-coverage_30 <- coverage[coverage[,2]>.3,]
+coverage_30 <- coverage[coverage[,2]>.3,]  # exclude variables that are missing more than 70 percent of the time
+
+#split data into training and test sets
+training_indices <- createDataPartition(modelling_data$diff_inBMD4_outBMD4,  #dependent variable
+                                        groups = 10,                          #stratified over 10 quintiles
+                                        p = 0.8,                              # 80 percent used for training data
+                                        list = F)                             # output should be indicies not a list
+total_train_data_tdiff <- modelling_data[training_indices,] #%>% filter(new_diff < 2 & new_diff >-2)
+test_data_tdiff <- modelling_data[-training_indices,]
+
+#split total training data into feature selection data and training data
+feature_indices <- createDataPartition(total_train_data_tdiff$diff_inBMD4_outBMD4,
+                                       p = 1/8,
+                                       list = F)
+feature_selection_data <- total_train_data_tdiff[feature_indices,]
+train_data_tdiff <- total_train_data_tdiff[-feature_indices,]
 
 
-#####################select variables to work with##############################
-set.seed(100)
-working_data_wm <- data %>% select(s_iso3c, r_iso3c, year, diff_inBMD4_outBMD4, re_diff_inBMD4_outBMD4, 
-                                   IN_BMD4,OUT_BMD4, rownames(coverage_30), re_OUT_BMD4, diff_fellow,
-                                   -Type, -re_IMF_IN, -re_OECD_IN_BMD4) %>%
-  mutate_if(is.character, as.factor) %>%
-  filter(!is.na(OUT_BMD4)) %>%    # Outward stocks data needs to be non-missing
-  group_by(s_iso3c, r_iso3c) %>%
-  mutate( lag1_diff = lag(diff_inBMD4_outBMD4, n=1, default=NA,order_by = year),
-          lag2_diff = lag(diff_inBMD4_outBMD4, n=2, default=NA,order_by = year),
-          lag3_diff = lag(diff_inBMD4_outBMD4, n=3, default=NA,order_by = year),
-          lag4_diff = lag(diff_inBMD4_outBMD4, n=4, default=NA,order_by = year),
-          lead1_diff = lead(diff_inBMD4_outBMD4, n=1, default=NA, order_by = year),
-          lead2_diff = lead(diff_inBMD4_outBMD4, n=2, default=NA, order_by = year),
-          lead3_diff = lead(diff_inBMD4_outBMD4, n=3, default=NA, order_by = year),
-          lead4_diff = lead(diff_inBMD4_outBMD4, n=4, default=NA, order_by = year),
-          lag1_rediff = lag(re_diff_inBMD4_outBMD4, n=1, default=NA,order_by = year),
-          lag2_rediff = lag(re_diff_inBMD4_outBMD4, n=2, default=NA,order_by = year),
-          lag3_rediff = lag(re_diff_inBMD4_outBMD4, n=3, default=NA,order_by = year),
-          lag4_rediff = lag(re_diff_inBMD4_outBMD4, n=4, default=NA,order_by = year),
-          lead1_rediff = lead(re_diff_inBMD4_outBMD4, n=1, default=NA, order_by = year),
-          lead2_rediff = lead(re_diff_inBMD4_outBMD4, n=2, default=NA, order_by = year),
-          lead3_rediff = lead(re_diff_inBMD4_outBMD4, n=3, default=NA, order_by = year),
-          lead4_rediff = lead(re_diff_inBMD4_outBMD4, n=4, default=NA, order_by = year)
-  ) %>%
-  ungroup()
+# #Select features
+# 
+# #numeric features with a correlation of at least 0.5
+# numeric_wd <- feature_selection_data %>% select(rownames(coverage_30), IN_BMD4, diff_inBMD4_outBMD4, new_diff) %>%
+#                                 select(where(is.numeric)) %>%
+#                                 cor(., use = "pairwise.complete.obs", method = "spearman")
+# correlation_2 <- rownames(numeric_wd[numeric_wd[,"new_diff"]>.5 |  numeric_wd[,"new_diff"] < -.5,])
+# correlation_2 <- correlation_2[correlation_2 != "new_diff" & correlation_2 != "IN_BMD4"  ]
+# 
+# 
+# #factor variables
+# 
+# #convert data to pair level
+# factor_vars <- feature_selection_data %>% select(starts_with("s_IMF"), starts_with("r_IMF"), des_pair) %>%
+#                    unique()
+# dep_factors_vars <- feature_selection_data %>% select(new_diff, des_pair) %>%
+#                     group_by(des_pair) %>%
+#                     summarize(new_diff = mean(new_diff, na.rm = T)) %>%
+#                     unique()
+# #convert the dependent variable into degrees deviation from the expected 45 degree line.
+# factor_vars <- cbind(factor_vars, dep_factors_vars[, "new_diff"])
+# 
+# 
+# #perform a kruskal test to assess whether at least one of the factor levels is assciated with different OUT-IN ratios
+# kruskal_results <-  lapply(factor_vars[,1:58], function(nm)
+#   kruskal.test(new_diff ~ nm, data = factor_vars)  #extracts p-value from levene-tests
+# )
+# kruskal <- data.frame(matrix(unlist(kruskal_results), nrow=length(kruskal_results), byrow=TRUE))
+# rownames(kruskal) <- colnames(factor_vars[,1:58])
+# names(kruskal)[names(kruskal) == "X3"] <- "pvalue"
+# kruskal$pvalue <- as.numeric(kruskal$pvalue)
+# kruskal <- kruskal %>% filter(pvalue<0.001)
+
+# # vector of selected features
+# selected_features <- c( coverage_30 ,rownames(kruskal), "lag1", "lag2", "OUT_BMD4") #correlation_2
+# 
+# # Algorithm based selection
+
+# imputation
+feature <- feature_selection_data %>% select(all_of(rownames(coverage_30)))
+knn_impute <- preProcess(as.data.frame(feature),
+                            method = c("knnImpute"),
+                            k = 20,
+                            knnSummary = mean)
+
+feature <- predict(knn_impute, newdata = feature , na.action = na.pass)
+feature_selection_data <- feature_selection_data %>% select(diff_inBMD4_outBMD4) %>%
+                                                     cbind(.,feature)
+
+
+# # fit control
+# selection_fitControl <- trainControl(method = "none",    # method for resampling 
+#                            savePredictions = F    # save prediction during fitting process
+#                            )
+# #glmnet
+# selection_glmnet <- train(diff_inBMD4_outBMD4 ~ .,
+#                           data = (feature_selection_data %>% select(all_of(rownames(coverage_30)), diff_inBMD4_outBMD4)),
+#                           method = "glmnet", 
+#                           tune_grid = expand.grid(alpha = 1),
+#                           trControl = selection_fitControl,
+#                           preProcess = c("knnImpute"),
+#                           na.action = na.pass
+#                           )
+# var_sel_glmnet <- varImp(selection_glmnet)$importance %>% arrange(desc(Overall)) %>% head(4) %>% rownames()
+# 
+# selection2_glmnet <- train(diff_inBMD4_outBMD4 ~ .,
+#                           data = feature_selection_data %>% select(starts_with("r_IMF"), 
+#                                                                    starts_with("s_IMF"),
+#                                                                    diff_inBMD4_outBMD4),
+#                           method = "glmnet", 
+#                           trControl = selection_fitControl,
+#                           preProcess = c("knnImpute"),
+#                           na.action = na.pass)
+# varImp(selection2_glmnet)$importance %>% arrange(desc(Overall))
+
+
+#interactionForest for detecting interaction effects
+
+selection_intforest <- interactionfor(diff_inBMD4_outBMD4 ~., 
+                                      data = (feature_selection_data %>% select(all_of(rownames(coverage_30)), diff_inBMD4_outBMD4)), 
+                                      importance = "both",
+                                      simplify.large.n = TRUE)  #adjusts tree depth and number of trees accroding to number of variables
+str_replace <- c(" AND "= "*", " small" = "", " large"="")
+quant_interactions <- as.data.frame(selection_intforest$eim.quant.sorted) %>% head(5) %>% 
+                                    rownames() %>% str_replace_all(.,str_replace) %>% gsub("[ ].*","" ,.)
+qual_interactions <- as.data.frame(selection_intforest$eim.qual.sorted) %>% head(5) %>% 
+                                   rownames() %>% str_replace_all(.,str_replace) %>% gsub("[ ].*","" ,.)
+uni_var <- as.data.frame(selection_intforest$eim.univ.sorted) %>% head(10) %>% 
+                                   rownames() %>% str_replace_all(.,str_replace) %>% gsub("[ ].*","" ,.)
+form <- c(uni_var, quant_interactions, qual_interactions) %>% unique() %>% 
+                                           paste(., collapse = " + ") %>% 
+                                           c("diff_inBMD4_outBMD4 ~ ", ., "-1") %>% 
+                                           paste(., collapse= "") %>%
+                                           as.formula()
 
 
 ###############Training data################
 
 #########training sample for total difference#############
 #exclude variables that should not be used for prediction
-train_data_tdiff <- working_data_wm %>% 
-  filter(!is.na(diff_inBMD4_outBMD4)) %>%                            #& OUT_BMD4!=0 & IN_BMD4!=0 & OUT_BMD4<10000)
-  select(-s_iso3c,-r_iso3c,-year,-IN_BMD4,-diff_inBMD4_outBMD4)
+dep_tdiff <- train_data_tdiff %>% select(diff_inBMD4_outBMD4, new_diff, s_iso3c, r_iso3c, year, IN_BMD4, OUT_BMD4)
 
-dep_tdiff <- working_data_wm %>%  filter(!is.na(diff_inBMD4_outBMD4)) %>% select(diff_inBMD4_outBMD4, s_iso3c, r_iso3c, year)
+train_data_tdiff <- model.matrix.lm(form, data = train_data_tdiff, na.action="na.pass") %>% as.matrix()
+
 
 #one-hot endcoding
-xgb_train_num <- train_data_tdiff %>% select(where(is.numeric))
-xgb_train_fac <- train_data_tdiff %>% select(where(is.factor))
-xgb_dummy <- dummyVars(" ~ .", data = xgb_train_fac)
-xgb_single_answers <- as.data.frame(predict(xgb_dummy, newdata = xgb_train_fac))
+#xgb_train_num <- train_data_tdiff %>% select(where(is.numeric))
+#xgb_train_fac <- train_data_tdiff %>% select(where(is.factor))
+#xgb_dummy <- dummyVars(" ~ .", data = xgb_train_fac)
+#xgb_single_answers <- as.data.frame(predict(xgb_dummy, newdata = xgb_train_fac))
 
 #combine to xgb Matrix
-xgb_train_tdiff <- cbind(xgb_train_num, xgb_single_answers) %>% as.matrix()
-xgb_train_tdiff_sp <- as(xgb_train_tdiff, "dgCMatrix")
+#xgb_train_tdiff <- cbind(xgb_train_num, xgb_single_answers) %>% as.matrix()
+xgb_train_tdiff_sp <- as(train_data_tdiff, "dgCMatrix")
 
 #######training sample for difference in fellows###########
 
-train_data_difffellow <- working_data_wm %>% 
-  filter(!is.na(diff_fellow)) %>%                            #& OUT_BMD4!=0 & IN_BMD4!=0 & OUT_BMD4<10000)
-  select(-s_iso3c,-r_iso3c,-year,-IN_BMD4,-diff_fellow)
+#train_data_difffellow <- working_data_wm %>% 
+#  filter(!is.na(diff_fellow)) %>%                            #& OUT_BMD4!=0 & IN_BMD4!=0 & OUT_BMD4<10000)
+#  select(-s_iso3c,-r_iso3c,-year,-IN_BMD4,-diff_fellow)
 
-dep_difffellow <- working_data_wm %>%  filter(!is.na(diff_fellow)) %>% select(diff_fellow, s_iso3c, r_iso3c, year)
+#dep_difffellow <- working_data_wm %>%  filter(!is.na(diff_fellow)) %>% select(diff_fellow, s_iso3c, r_iso3c, year) #, max_OUT_BMD4)
 
 #one-hot endcoding
-xgb_train_num <- train_data_difffellow %>% select(where(is.numeric))
-xgb_train_fac <- train_data_difffellow %>% select(where(is.factor))
-xgb_dummy <- dummyVars(" ~ .", data = xgb_train_fac)
-xgb_single_answers <- as.data.frame(predict(xgb_dummy, newdata = xgb_train_fac))
+#xgb_train_num <- train_data_difffellow %>% select(where(is.numeric))
+#xgb_train_fac <- train_data_difffellow %>% select(where(is.factor))
+#xgb_dummy <- dummyVars(" ~ .", data = xgb_train_fac)
+#xgb_single_answers <- as.data.frame(predict(xgb_dummy, newdata = xgb_train_fac))
 
 #combine to xgb Matrix
-xgb_train_difffellow <- cbind(xgb_train_num, xgb_single_answers) %>% as.matrix()
-xgb_train_difffellow_sp <- as(xgb_train_difffellow, "dgCMatrix")
+#xgb_train_difffellow <- cbind(xgb_train_num, xgb_single_answers) %>% as.matrix()
+#xgb_train_difffellow_sp <- as(xgb_train_difffellow, "dgCMatrix")
 
+################# test sample ################################
+dep_test_tdiff <- test_data_tdiff %>% select(diff_inBMD4_outBMD4, new_diff, s_iso3c, r_iso3c, year, IN_BMD4, OUT_BMD4)
+
+test_data_tdiff <- model.matrix.lm(form, data = test_data_tdiff, na.action="na.pass") %>% as.matrix()
+
+
+# #one-hot endcoding
+# xgb_test_num <- test_data_tdiff %>% select(where(is.numeric))
+# xgb_test_fac <- test_data_tdiff %>% select(where(is.factor))
+# xgb_dummy <- dummyVars(" ~ .", data = xgb_test_fac)
+# xgb_single_answers <- as.data.frame(predict(xgb_dummy, newdata = xgb_test_fac))
+# 
+# #combine to xgb Matrix
+# xgb_test_tdiff <- cbind(xgb_test_num, xgb_single_answers) %>% as.matrix()
+xgb_test_tdiff_sp <- as(test_data_tdiff, "dgCMatrix")
--- total_diff_train.R
+++ total_diff_train.R
@@ -9,14 +9,15 @@ fitControl <- trainControl(method = "repeatedcv",    # method for resampling
                            )
 
 #defines the tuning Grid
-customGrid <-  expand.grid(nrounds = c(200, 500, 1000), 
-                           max_depth = c(4,8,10), 
-                           eta = c(0.1, 0.2, 0.3),
+boost_grid <-  expand.grid(nrounds = c(100, 200, 400), 
+                           max_depth = c(5), 
+                           eta = c(0.1, 0.2),
                            gamma = 0,
                            colsample_bytree = 1,
                            min_child_weight = 0,
-                           subsample = c(0.4)
+                           subsample = c(1)
                            )
+
 
 
 #nrounds, max_depth, eta, gamma, colsample_bytree, min_child_weight, subsample
@@ -27,18 +28,18 @@ boost_tdiff_cv <- train(y = dep_tdiff$diff_inBMD4_outBMD4,
                  x = xgb_train_tdiff_sp, 
                  method = "xgbTree", 
                  trControl = fitControl,
-                 tuneGrid = customGrid,
+                 tuneGrid = boost_grid,
                  #other parameters to pass to xgboost training function:
                  objective = "reg:squarederror",
                  tree_method = "approx",
                  verbose = FALSE
                  )
-xgb.save(boost_tdiff_cv$finalModel, "boost_tdiff.model")
+#xgb.save(boost_tdiff_cv$finalModel, "boost_tdiff.model")
 importance_boost_tdiff <- varImp(boost_tdiff_cv)
 
 
 # k-fold crossvalidated forest
-customGrid <-  expand.grid(nrounds = 1, 
+forest_grid <-  expand.grid(nrounds = 1, 
                            max_depth = c(20,30), 
                            eta = 1,
                            gamma = 0,
@@ -51,7 +52,7 @@ forest_tdiff_cv <- train(y = dep_tdiff$diff_inBMD4_outBMD4,
                   x = xgb_train_tdiff_sp, 
                   method = "xgbTree", 
                   trControl = fitControl,
-                  tuneGrid = customGrid,
+                  tuneGrid = forest_grid,
                   #other parameters to pass to xgboost training function:
                   objective = "reg:squarederror",       #can be changed to "pseudohubererror"
                   tree_method = "approx",
@@ -59,77 +60,110 @@ forest_tdiff_cv <- train(y = dep_tdiff$diff_inBMD4_outBMD4,
                   num_parallel_tree = 300,              # Number of trees fitted per round -> can be used to simulate RF
                   verbose = FALSE
 )
-xgb.save(forest_tdiff_cv$finalModel, "forest_tdiff.model")
-importance_forest_tdiff <- varImp(forest_tdiff_cv)
+#xgb.save(forest_tdiff_cv$finalModel, "forest_tdiff.model")
+#importance_forest_tdiff <- varImp(forest_tdiff_cv)
+
+
+#k-fold elastic net 
+glmnet_grid <- expand.grid(alpha = c(0.1,0.5,0.9),
+                           lambda = c(0.01, 0.1 ,0.2))
+
+glmnet_tdiff_cv <- train(diff_inBMD4_outBMD4 ~ .,
+                          data = cbind(diff_inBMD4_outBMD4=dep_tdiff$diff_inBMD4_outBMD4, train_data_tdiff),
+                          method = "glmnet", 
+                          tune_grid = glmnet_grid,
+                          trControl = fitControl,
+                          preProcess = c("knnImpute"),
+                          na.action = na.pass
+                          )
 
 
 #save out-of-sample predictions
 prediction_train_tdiff <-data.frame(s_iso3c = dep_tdiff$s_iso3c,
                             r_iso3c = dep_tdiff$r_iso3c,
                             year = dep_tdiff$year,
-                            diff = dep_tdiff$diff_inBMD4_outBMD4)
+                            diff = dep_tdiff$diff_inBMD4_outBMD4,
+                            new_diff = dep_tdiff$new_diff,
+                            #weights= dep_tdiff$weights,
+                            IN_BMD4 = dep_tdiff$IN_BMD4,
+                            OUT_BMD4 = dep_tdiff$OUT_BMD4)#,
+                            #max=dep_tdiff$max_OUT_BMD4)
+
 
 # save predictions from cv-model
-boost_tdiff_pred <- data.frame(rowIndex= boost_tdiff_cv$pred$rowIndex[boost_tdiff_cv$pred$nrounds==200 & boost_tdiff_cv$pred$max_depth==4 & boost_tdiff_cv$pred$eta==0.1],
-                               pred= boost_tdiff_cv$pred$pred[boost_tdiff_cv$pred$nrounds==200 & boost_tdiff_cv$pred$max_depth==4 & boost_tdiff_cv$pred$eta==0.1])
+boost_tdiff_pred <- boost_tdiff_cv$pred %>% filter(nrounds == boost_tdiff_cv$bestTune$nrounds &
+                                                     max_depth == boost_tdiff_cv$bestTune$max_depth &
+                                                     eta == boost_tdiff_cv$bestTune$eta) %>% 
+                                            select(rowIndex, pred)
 
-forest_tdiff_pred <- data.frame(rowIndex= forest_tdiff_cv$pred$rowIndex[forest_tdiff_cv$pred$nrounds==1 & forest_tdiff_cv$pred$max_depth==30 & forest_tdiff_cv$pred$eta==1],
-                                pred= forest_tdiff_cv$pred$pred[forest_tdiff_cv$pred$nrounds==1 & forest_tdiff_cv$pred$max_depth==30 & forest_tdiff_cv$pred$eta==1])
+forest_tdiff_pred <- forest_tdiff_cv$pred %>% filter(nrounds == forest_tdiff_cv$bestTune$nrounds &
+                                                     max_depth == forest_tdiff_cv$bestTune$max_depth &
+                                                     eta == forest_tdiff_cv$bestTune$eta) %>% 
+                                            select(rowIndex, pred)
+
+glmnet_tdiff_pred <- glmnet_tdiff_cv$pred %>% filter(alpha == glmnet_tdiff_cv$bestTune$alpha & 
+                                                     lambda == glmnet_tdiff_cv$bestTune$lambda) %>%
+                                              select(rowIndex, pred)
+
+
+
 
 # combine with dependent variable
 prediction_train_tdiff$boost[boost_tdiff_pred$rowIndex] <- boost_tdiff_pred$pred      
 prediction_train_tdiff$forest[forest_tdiff_pred$rowIndex] <- forest_tdiff_pred$pred
+prediction_train_tdiff$glmnet[glmnet_tdiff_pred$rowIndex] <- glmnet_tdiff_pred$pred
 
 #################measure and compare performance###############################
 
-#performance measurement
-prediction_summary_tdiff <- prediction_train_tdiff %>% pivot_longer(cols = c(boost, forest), names_to = "method", values_to = "prediction") %>%
-  group_by(method) %>%
-  summarize(rmse = round(sqrt(mean((diff-prediction)^2)), digits = 0),
-            mae = round(mean(sqrt((diff-prediction)^2)), digits = 0),
-            pseudo_rsq = round(1-(sum((diff-prediction)^2)/sum((diff)^2)), digits=3),
-            N = n())
-write.csv(prediction_summary_tdiff, row.names=T)
+#performance on training set
+prediction_summary_tdiff <- rbind(getTrainPerf(boost_tdiff_cv),
+                                  getTrainPerf(forest_tdiff_cv),
+                                  getTrainPerf(glmnet_tdiff_cv))
+#write.csv(prediction_summary_tdiff, row.names=T)
+
+#performance on the test set
+# prediction_test_tdiff <- data.frame(boost = predict(boost_tdiff_cv, newdata =  xgb_test_tdiff_sp),
+#                                     forest = predict(forest_tdiff_cv , newdata = xgb_test_tdiff_sp)) %>%
+#                                     apply(., 2, postResample, obs = dep_test_tdiff$diff_inBMD4_outBMD4) %>%
+#                                     t()
+
 
 #quintile performance
 quin_perf_tdiff <- prediction_train_tdiff %>% 
-  mutate(quintile = ntile(boost, 10)) %>%
+  mutate(quintile = ntile(diff, 10)) %>%
   group_by(quintile) %>%
   summarise(
-    min = min(boost),
-    max = max(boost),
+    min = min(diff),
+    max = max(diff),
     Errorred = round(1-(sum(abs(diff-boost), na.rm=T))/sum(abs(diff), na.rm=T), digits=2),
     pRsquared = round(1-(sum((diff-boost)^2, na.rm=T)/sum((diff)^2, na.rm=T)), digits=2)
   )
 print(quin_perf_tdiff)
-write.csv(quin_perf_tdiff, row.names=T)
+#write.csv(quin_perf_tdiff, row.names=T)
 
 #graphical analysis of the performance
 
-graph_data_tdiff <- data %>% select(s_iso3c, r_iso3c, year, group_id, IN_BMD4, OUT_BMD4, fin_center) %>%
-                             merge(prediction_train_tdiff, by = c("s_iso3c", "r_iso3c", "year"), y.all = T ) %>%
-                             mutate(adj_OUT_BMD4=OUT_BMD4+boost, 
+graph_data_tdiff <- prediction_train_tdiff %>% select(s_iso3c, r_iso3c, year, IN_BMD4, boost, OUT_BMD4) %>% #, max_OUT_BMD4) %>%
+                             mutate(adj_OUT_BMD4=OUT_BMD4+boost,
                                     rel_error2=2*abs(IN_BMD4-OUT_BMD4)/(abs(IN_BMD4)+abs(OUT_BMD4)),
                                     adj_rel_error2=2*abs(IN_BMD4-adj_OUT_BMD4)/(abs(IN_BMD4)+abs(adj_OUT_BMD4)))
 
 # graph Inward versus outward, distinction by fin_center, comparison to prediction
-diff1 <- ggplot(data = graph_data_tdiff) + geom_jitter(aes(y=IN_BMD4, x=OUT_BMD4, color=rel_error2)) +
-                                     facet_wrap(~ fin_center, ncol = 2) #+
+diff1 <- ggplot(data = graph_data_tdiff) + geom_jitter(aes(y=IN_BMD4, x=OUT_BMD4)) #+
+                                     #facet_wrap(~ fin_center, ncol = 2) #+
 #coord_cartesian(xlim=c(-500,1000), ylim =c(-500,1000) )
 #labs(title = "Differences between inward and outward stocks",
 #      caption = "N=XX")
-adjust1 <- ggplot(data = graph_data_tdiff) + geom_jitter(aes(y=IN_BMD4, x=adj_OUT_BMD4, color=adj_rel_error2)) +
-                                       facet_wrap(~ fin_center, ncol = 2) #+
-# coord_cartesian(xlim=c(-500,1000), ylim =c(-500,1000) )
-plot_grid(diff1, adjust1, nrow = 2)                            
+adjust1 <- ggplot(data = graph_data_tdiff) + geom_jitter(aes(y=IN_BMD4, x=adj_OUT_BMD4)) #+
+                                       #facet_wrap(~ fin_center, ncol = 2) +
+#coord_cartesian(xlim=c(-500,1000), ylim =c(-500,1000) )
+plot_grid(diff1, adjust1, ncol = 2)                            
 
 
 #graph histogram of rel measurement error
-hist_diff1 <- ggplot(data = graph_data_tdiff) + geom_histogram(aes(x=rel_error2, y=after_stat(count/sum(count)*100))) +
-  facet_wrap(~ fin_center, ncol = 2) 
-hist_adjust1 <- ggplot(data =graph_data_tdiff) + geom_histogram(aes(x=adj_rel_error2, y=after_stat(count/sum(count)*100))) +
-  facet_wrap(~ fin_center, ncol = 2) 
+hist_diff1 <- ggplot(data = graph_data_tdiff) + geom_histogram(aes(x=rel_error2, y=after_stat(count/sum(count)*100))) #+
+  #facet_wrap(~ fin_center, ncol = 2) 
+hist_adjust1 <- ggplot(data =graph_data_tdiff) + geom_histogram(aes(x=adj_rel_error2, y=after_stat(count/sum(count)*100))) #+
+ # facet_wrap(~ fin_center, ncol = 2) 
 plot_grid(hist_diff1, hist_adjust1, nrow = 2)
-
-
 
