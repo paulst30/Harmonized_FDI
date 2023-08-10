996e6ab725e409e68a0865805c5904162459db80
paulst30 <paul.stricker@rub.de>
2023-08-10 13:53:30 +0200

    added graphical representation of harmonized dataset

--- practical_test.R
+++ practical_test.R
@@ -78,11 +78,104 @@ practical_test_data <- data %>% merge(.,prediction_obs, by = c("s_iso3c", "r_iso
 
 
 #### plausibility check #####
-practical_test_data[practical_test_data$fin_center==0 & practical_test_data$target=="OECD_IN_BMD3" ,] %>% group_by(year) %>% 
-               summarize(mean_normal=mean(IN_BMD4), mean_adjusted=mean(adjusted), mean_naive=mean(naive), sum_BIT=sum(BIT)) %>% 
-               ggplot() + geom_line(aes(y=mean_naive, x=year)) + geom_line(aes(y=mean_adjusted, x=year, color="adjusted")) + 
-               geom_line(aes(y=mean_normal, x=year, color="normal"))
+list_plot <- list()
+list_plot[[1]] <- practical_test_data[practical_test_data$fin_center==0 ,] %>% group_by(year) %>%
+                  summarize(naive = sum(naive),
+                            harmonized = sum(adjusted)) %>%
+                  pivot_longer(c(naive, harmonized), names_to = "series", values_to = "FDI") %>%
+                  ggplot(aes(x=year, y=FDI)) +  
+                  geom_line(aes(linetype=series)) +  theme(legend.position = "none")
 
+list_plot[[2]] <- practical_test_data[practical_test_data$fin_center==1 ,] %>% group_by(year) %>%
+                  summarize(naive = sum(naive),
+                            harmonized = sum(adjusted)) %>%
+                  pivot_longer(c(naive, harmonized), names_to = "series", values_to = "FDI") %>%
+                  ggplot(aes(x=year, y=FDI)) +  
+                  geom_line(aes(linetype=series)) + labs(linetype="Series") + theme(axis.title.y=element_blank()) #remove y axis labels
+  
+
+list_plot[[3]] <- practical_test_data[practical_test_data$fin_center==0 & !is.na(practical_test_data$target) ,] %>% 
+                  mutate(task = case_when(run==1 ~ "IN BMD4 -> IN BMD3",
+                                                     run==2 ~ "OUT BMD4 -> IN BMD3",
+                                                     run==3 ~ "OUT BMD3 -> IN BMD3",
+                                                     run==4 ~ "IN BMD4 -> OUT BMD3",
+                                                     run==5 ~ "OUT BMD4 -> OUT BMD3",
+                                                     run==6 ~ "OUT BMD4 -> IN BMD4")) %>%
+                  group_by(year, task) %>%
+                  summarize(diff = sum(adjusted-naive)) %>% 
+                  ggplot(aes(x=year, y=diff, fill=task)) + 
+                  geom_col(position = "stack") + theme(legend.position = "none") + labs(y="Adjustment")
+
+
+list_plot[[4]] <- practical_test_data[practical_test_data$fin_center==1 & !is.na(practical_test_data$target) ,] %>% 
+                  mutate(task = case_when(run==1 ~ "IN BMD4 -> IN BMD3",
+                                          run==2 ~ "OUT BMD4 -> IN BMD3",
+                                          run==3 ~ "OUT BMD3 -> IN BMD3",
+                                          run==4 ~ "IN BMD4 -> OUT BMD3",
+                                          run==5 ~ "OUT BMD4 -> OUT BMD3",
+                                          run==6 ~ "OUT BMD4 -> IN BMD4")) %>%
+                  group_by(year, task) %>%
+                  summarize(diff = sum(adjusted-naive)) %>% 
+                  ggplot(aes(x=year, y=diff, fill=task)) + 
+                  geom_col(position = "stack") + theme(axis.title.y=element_blank()) + labs(fill="Prediction task")
+
+#combine all graphs
+legend1 <- get_legend(list_plot[[2]])
+legend2 <- get_legend(list_plot[[4]])
+legends <- plot_grid(legend1,
+                     legend2,
+                     ncol=1, align="v")
+all <- plot_grid(list_plot[[1]]+ theme(axis.text.x=element_blank(),  #remove y axis labels
+                                       axis.ticks.x=element_blank(),
+                                       axis.title.x=element_blank()),  #remove y axis ticks,
+                 list_plot[[2]]+ theme(axis.text.x=element_blank(),  #remove y axis labels
+                                       axis.ticks.x=element_blank(),
+                                       axis.title.x=element_blank(),
+                                       legend.position = "none"),
+                 list_plot[[3]],
+                 list_plot[[4]]+ theme(legend.position = "none"),
+                 align = "v",
+                 labels = c("A","B"))
+plot_grid(all,
+          legends,
+          align = "vh",
+          ncol=2,
+          rel_widths = c(2.05, .6))
+
+# counting country pairs
+length(unique(practical_test_data$pair[practical_test_data$fin_center==1]))
+#5183
+length(unique(practical_test_data$pair[practical_test_data$fin_center==0]))
+#24863
+
+#### top 5 affected sender and receiver ####
+#sender
+stop5 <- practical_test_data %>% group_by(sender) %>% 
+                        summarize(naive2=sum(naive)/10000000,
+                                  harmonized=sum(adjusted)/10000000,
+                                  abs_adjustment=sum(abs(adjusted-naive)),
+                                  rel_abs_adjustment=round(sum(abs(adjusted-naive))/sum(abs(naive))*100, digits=1)) %>%
+                        arrange(desc(abs_adjustment)) %>%
+                        head(n=10)
+#receiver
+rtop5 <- practical_test_data %>% group_by(receiver) %>% 
+                        summarize(naive2=sum(naive)/10000000,
+                                  harmonized=sum(adjusted)/10000000,
+                                  abs_adjustment=sum(abs(adjusted-naive)),
+                                  rel_abs_adjustment=round(sum(abs(adjusted-naive))/sum(abs(naive))*100, digits=1)) %>%
+                        arrange(desc(abs_adjustment)) %>%
+                        head(n=10)
+
+#combine both
+top_5_affect <- cbind(stop5[-4],rtop5[-4])
+colnames(top_5_affect) <- c("Sender", "Naive", "Harmonized", "abs. Adjust.", "Receiver", "Naive", "Harmonized", "abs. Adjust.")
+write.csv(top_5_affect, row.names=F) #output for the paper
+
+### proportions of vintages
+harmonized_data %>% mutate(across(c("harmonized_vintage","naive_vintage"), ~ as.factor(.x))) %>% group_by(harmonized_vintage) %>%
+                    summarize(n = n()) %>% ungroup() %>% mutate(n=n/sum(n))
+harmonized_data %>% mutate(across(c("harmonized_vintage","naive_vintage"), ~ as.factor(.x))) %>% group_by(naive_vintage) %>%
+                    summarize(n = n()) %>% ungroup() %>% mutate(n=n/sum(n))
 
 #### practical research test #####
 
@@ -91,12 +184,12 @@ simple_model <- feols(fml = c(t_naive ,t_adjusted) ~ BIT + PTA + DTT   | pair + 
                       data = practical_test_data)
 
 simple_model_r <- feols(fml = c(t_naive ,t_adjusted) ~ BIT + PTA + DTT +
-                              r_sum_BIT + r_GDPcurr + r_Trade + r_GDPpercap  
+                              r_GDPcurr + r_Trade + r_GDPpercap  
                               | pair + receiver + sender^year + year, 
                         data = practical_test_data)
 
 simple_model_rs <- feols(fml = c(t_naive ,t_adjusted) ~ BIT + PTA + DTT  +
-                           r_sum_BIT + r_GDPcurr + r_Trade + r_GDPpercap +
+                           r_GDPcurr + r_Trade + r_GDPpercap +
                            s_sum_BIT + s_GDPcurr + s_Trade + s_GDPpercap
                         | pair + receiver + sender + year, 
                         data = practical_test_data)
